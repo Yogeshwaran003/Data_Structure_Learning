47ebd35e4ca998c4f880c2c5a60b94027101cbab
Yogeshwaran003 <yogiwar33@gmail.com>
2025-03-26 18:19:02 +0530

    Updated the search algo

--- Search.java
+++ Search.java
@@ -107,14 +107,28 @@ class Searcharray{
     public static int findB(int[] arr , int val){
         int start=0;
         int end = arr.length-1;
+        boolean asc = false;
+        if(arr[start] < arr[end]){
+            asc = true;
+        }
         while(start<=end){
             int mid = (start+end)/2;
-            if(val>arr[mid]){
-                start=mid+1;
-            }else if (val<arr[mid]){
-                end=mid-1;
+            if(asc){
+                if(val>arr[mid]){
+                    start=mid+1;
+                }else if (val<arr[mid]){
+                    end=mid-1;
+                }else{
+                    return mid;
+                }
             }else{
-                return mid;
+                if(val>arr[mid]){
+                    end=mid-1;
+                }else if(val<arr[mid]){
+                    start = mid+1;
+                }else{
+                    return mid;
+                }
             }
         }
         return -1;
@@ -123,7 +137,7 @@ class Searcharray{
 
 class Search{
     public static void main(String[] args){
-        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int[] arr = {9,8,7,6,5,4,3,2,1};
         int[][] arr1 = {{1,2},{3,4},{5,6},{7,8,9}};
         System.out.println(" ");
         System.out.println(Searcharray.find(arr, 6));
