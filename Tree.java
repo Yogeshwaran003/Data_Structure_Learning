b2d042ee047cfe7b0deb492bb887cedba089d2bc
Yogeshwaran003 <yogiwar33@gmail.com>
2025-03-28 18:52:28 +0530

    Updated the tree

--- Tree.java
+++ Tree.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class treesturcture{
-
+    Node root;
     class Node{
         int data;
         Node right;
@@ -14,11 +14,42 @@ class treesturcture{
         }
     }
 
+    treesturcture(int val){
+        root = new Node(val);
+    }
+
+    public void insertLeft(Node node , int val){
+        Node newnode = new Node(val);
+        node.left=newnode;
+    } 
+    public void insertRight(Node node , int val){
+        Node newnode = new Node(val);
+        node.right=newnode;
+    } 
+
+    public void display(Node root){
+        Node temp = root;
+        if(temp==null){
+            return;
+        }
+        System.out.print(temp.data+" ");
+        display(temp.left);
+        display(temp.right);
+        // System.out.println("");
+    }
+
 }
 
 class Tree{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-
+        treesturcture tree = new treesturcture(12);
+        tree.insertLeft(tree.root, 18);
+        tree.insertRight(tree.root, 24);
+        tree.insertLeft(tree.root.left, 30);
+        tree.insertRight(tree.root.left, 36);
+        tree.insertLeft(tree.root.right, 42);
+        tree.insertRight(tree.root.right, 48);
+        tree.display(tree.root);
     }
 }
