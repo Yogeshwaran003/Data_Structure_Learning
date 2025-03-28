704a04309980d14d48bc00676c93d6744e400070
Yogeshwaran003 <yogiwar33@gmail.com>
2025-03-28 08:19:59 +0530

    Updated the Tree file

---
+++ Tree.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class treesturcture{
+
+    class Node{
+        int data;
+        Node right;
+        Node left;
+        Node(int val){
+            data=val;
+            right=null;
+            left=null;
+
+        }
+    }
+
+}
+
+class Tree{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+    }
+}
